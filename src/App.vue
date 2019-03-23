0d8caede0832ecfa343940abe9b05bf0de4e1101
admin <visualjupiter@gmail.com>
2019-03-23 17:47:45 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <button @click="startLoading">loading</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    startLoading () {
+      this.$loading.show()
+
+      setTimeout(() => {
+        this.$loading.hide()
+      }, 4000)
+    }
+  }
+}
+</script>
